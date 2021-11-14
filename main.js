9f1cdc5360ca7272262f2f53f948a08a1020a91e
sreeharisl <sree.sreeharis.96@gmail.com>
2021-11-14 21:01:22 +0530

    1st commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', function(){
+    let billAmount = document.getElementById('bill-amount').value;
+    let tipPercentage = document.getElementById('tip-percentage').value;
+
+    
+    let tipAmount = document.getElementById('tip-amount').value = (billAmount/100) * tipPercentage;
+    document.getElementById('total-bill').value = parseFloat(billAmount) + parseFloat(tipAmount);
+
+})
+
+
+// "(billAmount / 100) * tipPercentage"
